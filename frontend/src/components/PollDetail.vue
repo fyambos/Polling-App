86d5f4e2457ece8c028c9715cf6981d560652d0c
fyambos <feliciteyambos@gmail.com>
2023-10-30 15:41:29 +0100

    Revert "affichage poll by id"
    This reverts commit 17b2945ae12fba7c1a8b3a58ce8a371ad5a071c2.

--- frontend/src/components/PollDetail.vue
+++ frontend/src/components/PollDetail.vue
@@ -1,43 +1,17 @@
-
-<script>
-const polls = [
-  {
-    id: '1',
-    question: 'What is your favorite color?',
-    options: ['Blue', 'Red', 'Green'],
-    votes: [0, 0, 0]
-  },
-  {
-    id: '2',
-    question: 'What is your favorite animal?',
-    options: ['Dog', 'Cat', 'Bird'],
-    votes: [0, 0, 0]
-  }
-];
-
-export default {
-  data() {
-    return {
-      poll: null
-    };
-  },
-  created() {
-    const pollId = this.$route.params.id;
-    this.poll = polls.find(poll => poll.id === pollId);
-  },
-  methods: {
-    vote(optionIndex) {
-      if (this.poll && optionIndex >= 0 && optionIndex < this.poll.options.length) {
-        this.poll.votes[optionIndex]++;
-      }
-    }
-  }
-};
+<script setup lang="ts">
+const pollId = 1
+const indexes = [1]
+const poll = {
+      id: '1',
+      question: 'Quelle est votre couleur préférée?',
+      options: ['Bleu', 'Rouge', 'Vert'],
+      votes: [0, 0, 0]
+}
 </script>
 
 <template>
   <div class="poll">
-    <div v-if="poll">
+    <div v-if="indexes.includes(pollId)">
       {{ poll.question }}
       <div v-for="option in poll.options">
         <ul>
@@ -48,10 +22,5 @@ export default {
       </div>
       <button @click="vote()">vote</button>
     </div>
-    <div v-else>
-      <p>Le sondage n'a pas été trouvé.</p>
-    </div>
   </div>
 </template>
-
-
